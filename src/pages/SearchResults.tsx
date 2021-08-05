687bb8a203c58756ed715d75d5e5fbe0cab2c70c
unknown <mantegnaguido@gamil.com>
2021-08-05 16:22:26 -0300

    Filters are done!

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -94,6 +94,63 @@ const SearchResults = () => {
 
     }
 
+    const inputRef = useRef(null)
+    let filteredArray: Data[] | undefined = [];
+
+    const handleFilters = (e: React.MouseEvent<HTMLParagraphElement>) => {
+        const {value, currentTarget, id} = e.target as typeof e.target & {
+            value: string;
+            currentTarget: HTMLParagraphElement;
+            id: string 
+        };
+        let inputs: HTMLInputElement[] = Array.from(document.querySelectorAll('.input-filter'));
+
+        let selectedFilters: string[] = inputs.filter(input => input.checked).map(input => input.id);
+
+        const filterFun = (value: string, arr: Data[] | undefined) => {
+            let newArr;
+            // if(value === 'gratis') return arr?.filter(item => item.shipping.free_shipping);
+            // if(value === 'new') return arr?.filter(item => item.condition === 'new');
+
+            switch (value) {
+                case 'gratis':
+                    newArr = arr?.filter(item => item.shipping.free_shipping);
+                    break;
+                case 'nuevo':
+                    newArr = arr?.filter(item => item.condition === 'new')
+                    break;
+                case 'usado':
+                    newArr = arr?.filter(item => item.condition !== 'new')
+                    break;
+                case 'sin interes':
+                    newArr = arr?.filter(item => item.installments.rate === 0)
+                    break;
+                case '0-6':
+                    newArr = arr?.filter(item => item.installments.quantity <= 6)
+                    break;
+                case '6 o mas':
+                    newArr = arr?.filter(item => item.installments.quantity > 6)
+                    break;
+                default:
+                    setFilteredData(data)
+                    break;
+            }
+            return newArr
+        }
+
+        
+        for (let index = 0; index < selectedFilters.length; index++) {
+            if(index === 0 ) {
+                filteredArray = filterFun(selectedFilters[index], data)
+            } else {
+                filteredArray = filterFun(selectedFilters[index], filteredArray)
+            }
+            
+        }
+
+        setFilteredData(filteredArray)
+    }
+
     const results = filteredData?.length === 0 ? data : filteredData
 
     return (
@@ -109,42 +166,48 @@ const SearchResults = () => {
                 </div>
                 <div>
                     <p className="fw-bold mb-1 fs-5 text-dark">Costo de envio</p>
-                    <p>Gratis
+                    <p onClick={handleFilters} data-info="gratis">Gratis
                         <span className="text-muted ms-1">
-                            ({data?.filter(item => item.shipping.free_shipping === true).length})
+                            ({data?.filter(item => item.shipping.free_shipping).length})
                         </span>
                     </p>
+                    <input className="input-filter" type="checkbox" name="gratis" id="gratis" onClick={handleFilters} ref={inputRef}/>
                 </div>
                 <div>
                     <p className="fw-bold mb-1 fs-5 text-dark">Condicion</p>
-                    <p className="mb-1">Nuevo 
+                    <p onClick={handleFilters} data-info="gratis" className="mb-1">Nuevo 
                         <span className="text-muted ms-1">
                             ({data?.filter(item => item.condition === 'new').length})
                         </span>
                     </p>
-                    <p>Usado 
+                    <input className="input-filter" type="checkbox" name="nuevo" id="nuevo" onClick={handleFilters} ref={inputRef}/>
+                    <p onClick={handleFilters}>Usado 
                         <span className="text-muted ms-1">
                             ({data?.filter(item => item.condition !== 'new').length})
                         </span>
                     </p>
+                    <input className="input-filter" type="checkbox" name="usado" id="usado" onClick={handleFilters} ref={inputRef}/>
                 </div>
                 <div>
                     <p className="fw-bold mb-1 fs-5 text-dark">Pago</p>
-                    <p  className="mb-1">Sin interés 
+                    <p onClick={handleFilters} className="mb-1">Sin interés 
                         <span className="text-muted ms-1">
                             ({data?.filter(item => item.installments.rate === 0).length})
                         </span>
                     </p>
-                    <p  className="mb-1">0-6 
+                    <input className="input-filter" type="checkbox" name="sin interes" id="sin interes" onClick={handleFilters} ref={inputRef}/>
+                    <p onClick={handleFilters} className="mb-1">0-6 
                         <span className="text-muted ms-1">  
                             ({data?.filter(item => item.installments.quantity <= 6).length})
                         </span>    
                     </p>
-                    <p>12 o más 
+                    <input className="input-filter" type="checkbox" name="0-6" id="0-6" onClick={handleFilters} ref={inputRef}/>
+                    <p onClick={handleFilters}>12 o más 
                         <span className="text-muted ms-1">
                             {data?.filter(item => item.installments.quantity > 6).length}
                         </span> 
                     </p>
+                    <input className="input-filter" type="checkbox" name="12 0 mas" id="12 0 mas" onClick={handleFilters} ref={inputRef}/>
                 </div>
             </div>
 
