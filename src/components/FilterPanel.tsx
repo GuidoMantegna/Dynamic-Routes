defe9968493d2a520177f870a590444df0f170df
unknown <mantegnaguido@gamil.com>
2021-08-06 11:57:14 -0300

    Filter panel component were done

--- src/components/FilterPanel.tsx
+++ src/components/FilterPanel.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import '../pages/styles/SearchResults.scss';
 import { Data } from '../interfaces/Data';
 
 type Props = {
@@ -9,7 +9,7 @@ type Props = {
 
 const FilterPanel: React.FC<Props> = ({ results, data, handleFilters }) => {
 
-    const filterFunc = (value: string, arr: Data[] | undefined) => {
+    const filterFunc = (value: string | undefined, arr: Data[] | undefined) => {
         let newArr;
 
         switch (value) {
@@ -32,19 +32,18 @@ const FilterPanel: React.FC<Props> = ({ results, data, handleFilters }) => {
                 newArr = arr?.filter(item => item.installments.quantity > 6)
                 break;
             default:
-                // setFilteredData(data)
+                handleFilters(data)
                 break;
         }
         return newArr
     }
 
-    const inputRef = useRef(null)
     let filteredArray: Data[] | undefined = [];
 
     const handleClick = (e: React.MouseEvent) => {
 
         let inputs: HTMLInputElement[] = Array.from(document.querySelectorAll('.input-filter'));
-        let selectedFilters: string[] = inputs.filter(input => input.checked).map(input => input.id);
+        let selectedFilters: string[]  = inputs.filter(input => input.checked).map(input => input.id);
         
         for (let index = 0; index < selectedFilters.length; index++) {
             if(index === 0 ) {
@@ -53,74 +52,96 @@ const FilterPanel: React.FC<Props> = ({ results, data, handleFilters }) => {
                 filteredArray = filterFunc(selectedFilters[index], filteredArray)
             } 
         }
+        console.log(filteredArray)
 
         handleFilters(filteredArray)
     }
+
     return (
+        <>
+        <div className="filter-option-container">
+            <p className="filter-title">Search Title</p>
+            
+            <div>
+                <span className="filter-qty ms-0 me-2">{results?.length}</span>  
+                <label className="filter-option" htmlFor="resultado">Resultados</label>
+            </div>
+        </div>
 
-            <>
-                <div>
-                    <p className="fw-bold mb-1 fs-5 text-dark">Search Title</p>
-                    <div>
-                        <label htmlFor="resultado">Resultados</label>
-                        <span className="text-muted me-1">{results?.length}</span>  
-                    </div>
-                </div>
+        <div className="filter-option-container">
+            <p className="filter-title">Costo de envio</p>
+            <div>
+                <label className="filter-option">Gratis</label>
+                <span className="filter-qty">
+                    ({filterFunc('gratis', data)?.length})
+                </span>
+                <input className="input-filter" type="checkbox" name="gratis" id="gratis" onClick={handleClick} />
+                {/* <div className="form-check form-switch">
+                    <input className="form-check-input input-filter" type="checkbox" data-info="gratis" id="flexSwitchCheckDefault" onClick={handleClick}></input>
+                </div> */}
+            </div>
+        </div>
 
-                <div>
-                    <p className="fw-bold mb-1 fs-5 text-dark">Costo de envio</p>
-                    <div>
-                        <label>Gratis</label>
-                        <span className="text-muted ms-1">
-                            ({data?.filter(item => item.shipping.free_shipping).length})
-                        </span>
-                        <input className="input-filter" type="checkbox" name="gratis" id="gratis" onClick={handleClick} ref={inputRef}/>
-                    </div>
-                </div>
+        <div className="filter-option-container">
+            <p className="filter-title">Condicion</p>
 
-                <div>
-                    <p className="fw-bold mb-1 fs-5 text-dark">Condicion</p>
-                    <div>
-                        <label>Nuevo</label>
-                        <span className="text-muted ms-1">
-                            ({data?.filter(item => item.condition === 'new').length})
-                        </span>
-                        <input className="input-filter" type="checkbox" name="nuevo" id="nuevo" onClick={handleClick} ref={inputRef}/>
-                    </div>
-                    <div>
-                        <label>Usado</label> 
-                        <span className="text-muted ms-1">
-                            ({data?.filter(item => item.condition !== 'new').length})
-                        </span>
-                        <input className="input-filter" type="checkbox" name="usado" id="usado" onClick={handleClick} ref={inputRef}/>
-                    </div>
-                </div>
+            <div>
+                <label className="filter-option">Nuevo</label>
+                <span className="filter-qty">
+                    ({filterFunc('nuevo', data)?.length})
+                </span>
+                <input className="input-filter" type="checkbox" name="nuevo" id="nuevo" onClick={handleClick} />
+                {/* <div className="form-check form-switch">
+                    <input className="form-check-input input-filter" type="checkbox" data-info="nuevo" id="flexSwitchCheckDefault" onClick={handleClick}></input>
+                </div> */}
+            </div>
+            <div>
+                <label className="filter-option">Usado</label> 
+                <span className="filter-qty">
+                    ({filterFunc('usado', data)?.length})
+                </span>
+                <input className="input-filter" type="checkbox" name="usado" id="usado" onClick={handleClick}/>
+                {/* <div className="form-check form-switch">
+                    <input className="form-check-input input-filter" type="checkbox" data-info="usado" id="flexSwitchCheckDefault" onClick={handleClick}></input>
+                </div> */}
+            </div>
+        </div>
 
-                <div>
-                    <p className="fw-bold mb-1 fs-5 text-dark">Pago</p>
-                    <div>
-                        <label>Sin interés</label> 
-                        <span className="text-muted ms-1">
-                            ({data?.filter(item => item.installments.rate === 0).length})
-                        </span>
-                        <input className="input-filter" type="checkbox" name="sin interes" id="sin interes" onClick={handleClick} ref={inputRef}/>
-                    </div>
-                    <div>
-                        <label>0-6</label>
-                        <span className="text-muted ms-1">  
-                            ({data?.filter(item => item.installments.quantity <= 6).length})
-                        </span>    
-                        <input className="input-filter" type="checkbox" name="0-6" id="0-6" onClick={handleClick} ref={inputRef}/>
-                    </div>
-                    <div>
-                        <label>12 o más</label> 
-                        <span className="text-muted ms-1">
-                            {data?.filter(item => item.installments.quantity > 6).length}
-                        </span> 
-                        <input className="input-filter" type="checkbox" name="12 0 mas" id="12 0 mas" onClick={handleClick} ref={inputRef}/>
-                    </div>
-                </div>
-                </>
+        <div className="filter-option-container">
+            <p className="filter-title">Pago</p>
+
+            <div>
+                <label className="filter-option">Sin interés</label> 
+                <span className="filter-qty">
+                    ({filterFunc('sin interes', data)?.length})
+                </span>
+                <input className="input-filter" type="checkbox" name="sin interes" id="sin interes" onClick={handleClick} />
+                {/* <div className="form-check form-switch">
+                    <input className="form-check-input input-filter" type="checkbox" data-info="sin interes" id="flexSwitchCheckDefault" onClick={handleClick}></input>
+                </div> */}
+            </div>
+            <div>
+                <label className="filter-option">0-6</label>
+                <span className="filter-qty">  
+                    ({filterFunc('0-6', data)?.length})
+                </span>    
+                <input className="input-filter" type="checkbox" name="0-6" id="0-6" onClick={handleClick} />
+                {/* <div className="form-check form-switch">
+                    <input className="form-check-input input-filter" type="checkbox" data-info="0-6" id="flexSwitchCheckDefault" onClick={handleClick}></input>
+                </div> */}
+            </div>
+            <div>
+                <label className="filter-option">12 o más</label> 
+                <span className="filter-qty">
+                    ({filterFunc('12 o mas', data)?.length})
+                </span> 
+                <input className="input-filter" type="checkbox" name="12 0 mas" id="12 o mas" onClick={handleClick} />
+                {/* <div className="form-check form-switch">
+                    <input className="form-check-input input-filter" type="checkbox" data-info="12 o mas" id="flexSwitchCheckDefault" onClick={handleClick}></input>
+                </div> */}
+            </div>
+        </div>
+        </>
 
     );
 };
--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import './styles/SearchResults.scss';
 import { Data } from '../interfaces/Data'
 import FilterPanel from '../components/FilterPanel'
+import SorterPanel from '../components/SorterPanel';
 
 const SearchResults = () => {
 
@@ -22,47 +23,11 @@ const SearchResults = () => {
         setData(data.results)
     }
 
-    let isOpen = false;
-
-    const handleSorting = (e: React.MouseEvent<HTMLSelectElement>) => {
-        const {value} = e.target as typeof e.target & {
-            value: string;
-        };
-
-        
-        if (!isOpen) {
-            isOpen = true;
-        } else {
-            switch (value) {
-                case 'mas-vendido':
-                    setFilteredData(() => {
-                        return data?.sort((a, b) => b.sold_quantity - a.sold_quantity)
-                    })
-                    break;
-                case 'menos-vendido':
-                    setFilteredData(() => {
-                        return data?.sort((a, b) => a.sold_quantity - b.sold_quantity)
-                    })
-                    break;
-                case 'menor-precio':
-                    setFilteredData(() => {
-                        return data?.sort((a, b) => a.price - b.price)
-                    })
-                    break;            
-                default:
-                    setFilteredData(() => {
-                        return data?.sort((a, b) => b.price - a.price)
-                    })
-                    break;
-            }
-
-            isOpen = false
-        } 
-
-
+    const handleFilters = (data: Data[]) => {
+        setFilteredData(data)
     }
 
-    const handleFilters = (data: Data[]) => {
+    const handleSorters = (data: Data[]) => {
         setFilteredData(data)
     }
 
@@ -71,43 +36,14 @@ const SearchResults = () => {
     return (
         <>
         <div className="row">
-            <div className="results-filter col-3 ps-5 pe-0 mt-3">
+            <div className="col-3 ps-4 pe-0 mt-3">
                 <FilterPanel results={results} data={data} handleFilters={handleFilters}/>
             </div>
 
-        <div className="col-9">
-            <div className="container">
-                <div className="results-sort mb-1">
-                    <label htmlFor="ordenar-por" className="strong">Ordenar por</label>
-                    <select onClick={handleSorting} name="ordenar-por" id="sorting-options" className="border-0 ms-2">
-                        <option  value="mayor-precio">Mayor precio</option>
-                        <option value="menor-precio">Menor precio</option>
-                        <option value="mas-vendido">Mas vendido</option>
-                        <option value="menos-vendido">Menos vendido</option>
-                    </select>
-                </div>
-                <ul className="results-list">
-                    {results?.map(item => {
-                        return (
-                            <a href={item.permalink} className="results-item bg-white p-3 border-bottom" key={item.id}>
-
-                                    <div className="item-img">
-                                        <img className="img-fluid" src={item.thumbnail} alt="item-img" />
-                                    </div>
-                                    <div className="item-info row align-items-around ps-5">
-                                        <h5 className="fw-light">{item.title}</h5>
-                                        <span className="h5 strong">${item.price}</span>
-                                        <p className="lead fs-6">vendidos: <span>{item.sold_quantity}</span></p>
-                                    </div>
-
-                            </a>
-                        )
-                    })}
-                </ul>
+            <div className="col-9">
+                <SorterPanel results={results} data={data} handleSorters={handleSorters} />
             </div>
         </div>
-        </div>
-        
         </>
     );
 };
